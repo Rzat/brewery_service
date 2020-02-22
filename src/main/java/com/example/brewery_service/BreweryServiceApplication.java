fb1aadf31d28e813e980657cb54e1e98c4cebd48
rajatthakur00@gmail.com <rajat@55>
2020-02-22 12:50:06 +0530

    Initial commit

---
+++ src/main/java/com/example/brewery_service/BreweryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.brewery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryServiceApplication.class, args);
+        System.out.println("****Starting**");
+    }
+
+}
