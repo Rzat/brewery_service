133e1fe35652f685c69c630879adb9cab75fe2cb
rajatthakur00@gmail.com <rajat@55>
2020-02-26 22:03:42 +0530

    Implementing mapstruct

---
+++ src/main/java/com/example/brewery_service/web/mapper/BeerMapper.java
@@ -0,0 +1,13 @@
+package com.example.brewery_service.web.mapper;
+
+
+import com.example.brewery_service.domain.Beer;
+import com.example.brewery_service.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+    BeerDto beerToBeerDto(BeerDto beerDto);
+
+    Beer beerDtoToBeer(Beer beer);
+}
