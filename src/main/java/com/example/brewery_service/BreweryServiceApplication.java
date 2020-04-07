488b7925c9ca92eb749864ca8056031e969dfc32
rajatthakur00@gmail.com <rajat@55>
2020-04-08 00:28:30 +0530

    configure beerservice for localmysql

--- src/main/java/com/example/brewery_service/BreweryServiceApplication.java
+++ src/main/java/com/example/brewery_service/BreweryServiceApplication.java
@@ -2,8 +2,9 @@ package com.example.brewery_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class BreweryServiceApplication {
 
     public static void main(String[] args) {
