665c901d5f85f954d90cce4620ee5a1546ab51a9
rajatthakur00@gmail.com <rajat@55>
2020-04-12 15:27:58 +0530

    Updated Configuring JMS for Services

--- src/main/java/com/example/brewery_service/config/JmsConfig.java
+++ src/main/java/com/example/brewery_service/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package com.example.brewery_service.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
@@ -8,6 +9,7 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
+    @Bean
     public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
